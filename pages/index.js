5e72297ecd380e7df7c48a7c128ec771c684e23d
franco66 <david_franco@outlook.com>
2023-11-07 14:29:01 -0300

    gg

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
 
 
 export default function Home({propertiesForSale,propertiesForRent}) {
-  //console.log(propertiesForSale,propertiesForRent)
+  //console.log(propertiesForSale,propertiesForRent ss)
 
   return (
     <Box>
